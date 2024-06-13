17a91a480b06d0494d21cc8e63b78f5b36948a1a
Satwik Garg <141859449+gargsatwik@users.noreply.github.com>
2024-06-14 00:53:46 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,37 +3,24 @@ import Footer from "./Footer.jsx";
 import Note from "./Note.jsx";
 import CreateArea from "./CreateArea.jsx";
 import React from "react";
-import axios from "axios";
-import { v4 as uuidv4 } from "uuid";
 
-const port = 5000;
-async function App() {
-  const noteList = await axios.get(`http://localhost:${port}/all-notes`);
+function App() {
+  const [noteList, changeNoteList] = React.useState([]);
 
-  async function addNote(titleText, contentText) {
+  function addNote(titleText, contentText) {
     const newNote = {
-      _id: uuidv4(),
       title: titleText,
       content: contentText,
     };
-    console.log(newNote);
-    const responseCode = await axios.post(
-      `http://localhost:${port}/new-note`,
-      newNote
-    );
-    if (responseCode !== 200) {
-      console.log("Error posting note");
-    }
+    changeNoteList([...noteList, newNote]);
   }
 
-  async function deleteNote(id) {
-    const statusCode = await axios.post(
-      `http://localhost:${port}/delete-note`,
-      id
-    );
-    if (statusCode !== 200) {
-      console.log("Failed to delete note");
-    }
+  function deleteNote(id) {
+    changeNoteList((prevList) => {
+      return prevList.filter((note, index) => {
+        return index !== id;
+      });
+    });
   }
 
   return (
@@ -44,7 +31,7 @@ async function App() {
         return (
           <Note
             key={index}
-            id={note._id}
+            id={index}
             title={note.title}
             content={note.content}
             delete={deleteNote}
